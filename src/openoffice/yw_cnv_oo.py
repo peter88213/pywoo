31747519d3ab8e653769815fd62bc6034e4dfab6
peter88213 <peter.88213@t-online.de>
2021-03-16 12:34:46 +0100

    Prepare v0.32.1 based on PyWriter v2.12.0
    Convert scene list from yw7 to ods spreadsheet.

--- src/openoffice/yw_cnv_oo.py
+++ src/openoffice/yw_cnv_oo.py
@@ -1,6 +1,6 @@
 """Import and export yWriter data (OpenOffice 3/4 variant).
 
-Copyright (c) 2020 Peter Triesberger
+Copyright (c) 2021 Peter Triesberger
 For further information see https://github.com/peter88213/PyWriter
 Published under the MIT License (https://opensource.org/licenses/mit-license.php)
 """
