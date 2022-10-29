24134e01f5247d71ef58671e022a12a028a39862
peter88213 <peter.88213@t-online.de>
2022-10-29 21:26:31 +0200

    Prepare for PyWriter v8
    Revise error handling:
    - Throw exceptions instead of returning error messages.

--- src/pywoolib/converter.py
+++ src/pywoolib/converter.py
@@ -4,7 +4,7 @@ Copyright (c) 2022 Peter Triesberger
 For further information see https://github.com/peter88213/pywoo
 Published under the MIT License (https://opensource.org/licenses/mit-license.php)
 """
-from pywriter.pywriter_globals import ERROR
+from pywriter.pywriter_globals import *
 from pywriter.converter.yw7_converter import Yw7Converter
 
 
@@ -28,10 +28,11 @@ class Converter(Yw7Converter):
         Show only error messages.
         Overrides the super class method.
         """
-        message = self.convert(source, target)
-        if message.startswith(ERROR):
+        try:
+            self.convert(source, target)
+        except Error as ex:
             self.newFile = None
-            self.ui.set_info_how(message)
+            self.ui.set_info_how(f'{ERROR}{str(ex)}')
         else:
             self.newFile = target.filePath
             self._open_newFile()
