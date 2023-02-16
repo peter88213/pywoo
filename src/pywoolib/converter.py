f0d220ac7f5c9add2bbe84dc80ee0eb8d5e92c8d
peter88213 <peter.88213@t-online.de>
2023-02-16 08:51:15 +0100

    Prepare v1.34.0 based on PyWriter v12.1.1
    - When exporting to yw7, save the document only if modified.
    - Speed up the process by converting the ODT/ODS document instead of an
    intermediate HTML/CSV file.

--- src/pywoolib/converter.py
+++ src/pywoolib/converter.py
@@ -1,11 +1,13 @@
 """Provide a converter class for universal import and export. 
 
-Copyright (c) 2022 Peter Triesberger
+Copyright (c) 2023 Peter Triesberger
 For further information see https://github.com/peter88213/pywoo
 Published under the MIT License (https://opensource.org/licenses/mit-license.php)
 """
 from pywriter.pywriter_globals import *
 from pywriter.converter.yw7_converter import Yw7Converter
+
+from pywriter.model.novel import Novel
 
 
 class Converter(Yw7Converter):
@@ -29,8 +31,12 @@ class Converter(Yw7Converter):
         Overrides the super class method.
         """
         try:
-            self.convert(source, target)
-        except Error as ex:
+            self.check(source, target)
+            source.novel = Novel()
+            source.read()
+            target.novel = source.novel
+            target.write()
+        except Exception as ex:
             self.newFile = None
             self.ui.set_info_how(f'!{str(ex)}')
         else:
