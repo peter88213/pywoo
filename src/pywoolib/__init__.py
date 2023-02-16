1d6f4ebcd99e21342840ef7206a4f0b55590d878
peter88213 <peter.88213@t-online.de>
2023-02-16 08:58:34 +0100

    Update docstrings

--- src/pywoolib/__init__.py
+++ src/pywoolib/__init__.py
@@ -4,7 +4,7 @@ Modules:
 
 converter -- Provide a converter class for universal import and export. 
 
-Copyright (c) 2022 Peter Triesberger
+Copyright (c) 2023 Peter Triesberger
 For further information see https://github.com/peter88213/pywoo
 Published under the MIT License (https://opensource.org/licenses/mit-license.php)
 """
